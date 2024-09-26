34984b00b21da95b789abb9c98d3185affce6dfd
Joao <joaovictorrfcontato@gmail.com>
2024-09-26 00:25:15 -0300

    Exercício Exceções

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using ContaBancaria.Entities;
+using ContaBancaria.Entities.Exceptions;
+using System;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);    
+
+
+            
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Account account = new Account(number, holder, balance, withdrawLimit);
+            try
+            {
+                account.Withdraw(amount);
+                Console.WriteLine("New balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException e) 
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+        }
+    }
+}
